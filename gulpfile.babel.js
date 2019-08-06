ead60cd277d70a68d5a3ab207c3fb346fd85860c
Khoa Lam <lakhoa1989@gmail.com>
2019-08-06 16:39:52 +0700

    wip test browser sync for xml

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -14,6 +14,7 @@ import webpack2      from 'webpack';
 import named         from 'vinyl-named';
 import uncss         from 'uncss';
 import autoprefixer  from 'autoprefixer';
+import xslt          from 'gulp-xslt';
 import rename        from 'gulp-rename';
 // Load all Gulp plugins into one variable
 const $ = plugins();
@@ -34,8 +35,9 @@ function loadConfig() {
 gulp.task('build',
  gulp.series(
               clean
-              , gulp.parallel(pages, javascript, images, copy)
-              , sass
+              , gulp.parallel(pages)
+              //, gulp.parallel(pages, javascript, images, copy)
+              //, sass
               //, styleGuide
             )
           );
@@ -66,6 +68,13 @@ function pages() {
       partials: 'src/partials/',
       data: 'src/data/',
       helpers: 'src/helpers/'
+    }))
+    .pipe(rename(function (path) {
+      // { dirname: 'huynhtan', basename: 'index', extname: '.xml' }
+      console.log(path);
+      // let filename = path.basename.split('.');
+      // path.basename = filename[0];
+      // path.extname = "." + filename[1];
     }))
     .pipe(gulp.dest(PATHS.dist));
 }
@@ -155,7 +164,8 @@ function images() {
 // Start a server with BrowserSync to preview the site in
 function server(done) {
   browser.init({
-    server: PATHS.dist, port: PORT
+    server: PATHS.server,
+    port: PORT
   }, done);
 }
 
@@ -167,13 +177,17 @@ function reload(done) {
 
 // Watch for changes to static assets, pages, Sass, and JavaScript
 function watch() {
-  gulp.watch(PATHS.assets, copy);
-  gulp.watch('src/pages/**/*.{html,xml,xslt}').on('all', gulp.series(pages, browser.reload));
+  // gulp.watch(PATHS.assets, copy);
+  gulp.watch('src/pages/**/*.{html,xml,xslt}').on('all', gulp.series(pages, function (done) {
+    browser.reload();
+    done();
+  }));
   gulp.watch('src/{layouts,partials}/**/*.html').on('all', gulp.series(resetPages, pages, browser.reload));
   gulp.watch('src/data/**/*.{js,json,yml}').on('all', gulp.series(resetPages, pages, browser.reload));
   gulp.watch('src/helpers/**/*.js').on('all', gulp.series(resetPages, pages, browser.reload));
-  gulp.watch('src/assets/scss/**/*.scss').on('all', sass);
-  gulp.watch('src/assets/js/**/*.js').on('all', gulp.series(javascript, browser.reload));
-  gulp.watch('src/assets/img/**/*').on('all', gulp.series(images, browser.reload));
+
+  // gulp.watch('src/assets/scss/**/*.scss').on('all', sass);
+  // gulp.watch('src/assets/js/**/*.js').on('all', gulp.series(javascript, browser.reload));
+  // gulp.watch('src/assets/img/**/*').on('all', gulp.series(images, browser.reload));
   // gulp.watch('src/styleguide/**').on('all', gulp.series(styleGuide, browser.reload));
 }
