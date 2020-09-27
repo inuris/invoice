3de3d81c8a8bc8c78b7a2fec133a840e17bac559
Khoa Lam <lakhoa1989@gmail.com>
2020-09-27 22:59:00 +0700

    test - html2pdf

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,6 +1,6 @@
 import $ from 'jquery';
 import 'what-input';
-
+import 'what-input';
 // Foundation JS relies on a global varaible. In ES6, all imports are hoisted
 // to the top of the file so if we used`import` to import Foundation,
 // it would execute earlier than we have assigned the global variable.
