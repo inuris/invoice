9a06f5d0d52c0e3cdc808aa9b71ed9a9ed1a5247
Khoa Lam <lakhoa1989@gmail.com>
2020-12-25 11:45:54 +0700

    add SachThietBiTruongHoc

---
+++ src/pages/sachthietbitruonghoc/copy.bat
@@ -0,0 +1,3 @@
+md "D:\Nodejs\VNPT-MauHoaDonDienTu\201225_SachThietBiTruongHoc_V5_AMTrong"
+copy "..\..\..\dist\sachthietbitruonghoc\sachthietbitruonghoc.xml" "D:\Nodejs\VNPT-MauHoaDonDienTu\201225_SachThietBiTruongHoc_V5_AMTrong"
+copy "..\..\..\dist\sachthietbitruonghoc\sachthietbitruonghoc.xslt" "D:\Nodejs\VNPT-MauHoaDonDienTu\201225_SachThietBiTruongHoc_V5_AMTrong"
