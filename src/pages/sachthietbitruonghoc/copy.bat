489ef138e4e12221c553e7f6e6f4de9885720de0
Khoa Lam <lakhoa1989@gmail.com>
2020-12-26 05:23:53 +0700

    update sachthietbitruonghoc cmd

--- src/pages/sachthietbitruonghoc/copy.bat
+++ src/pages/sachthietbitruonghoc/copy.bat
@@ -1,3 +1,4 @@
-md "D:\Nodejs\VNPT-MauHoaDonDienTu\201225_SachThietBiTruongHoc_V5_AMTrong"
-copy "..\..\..\dist\sachthietbitruonghoc\sachthietbitruonghoc.xml" "D:\Nodejs\VNPT-MauHoaDonDienTu\201225_SachThietBiTruongHoc_V5_AMTrong"
-copy "..\..\..\dist\sachthietbitruonghoc\sachthietbitruonghoc.xslt" "D:\Nodejs\VNPT-MauHoaDonDienTu\201225_SachThietBiTruongHoc_V5_AMTrong"
+set /p rootPublish=<..\..\..\copy.cfg
+md "%rootPublish%\201225_SachThietBiTruongHoc_V5_AMTrong"
+copy "..\..\..\dist\sachthietbitruonghoc\sachthietbitruonghoc.xml" "%rootPublish%\201225_SachThietBiTruongHoc_V5_AMTrong"
+copy "..\..\..\dist\sachthietbitruonghoc\sachthietbitruonghoc.xslt" "%rootPublish%\201225_SachThietBiTruongHoc_V5_AMTrong"
