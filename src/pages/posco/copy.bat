cf05554f321da6251fb48f35b26b0c20344d0e9c
Khoa Lam <lakhoa1989@gmail.com>
2020-12-29 08:30:58 +0700

    update Posco

---
+++ src/pages/posco/copy.bat
@@ -0,0 +1,10 @@
+set /p rootPublish=<..\..\..\copy.cfg
+md "%rootPublish%\201117_Posco_V5_AMTrong"
+copy "..\..\..\dist\posco\posco.xml" "%rootPublish%\201117_Posco_V5_AMTrong"
+copy "..\..\..\dist\posco\posco.xslt" "%rootPublish%\201117_Posco_V5_AMTrong"
+echo Set oWS = WScript.CreateObject("WScript.Shell")>CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut("201117_Posco_V5_AMTrong.lnk")>>CreateShortcut.vbs
+echo oLink.TargetPath = "%rootPublish%\201117_Posco_V5_AMTrong">>CreateShortcut.vbs
+echo oLink.Save>>CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
