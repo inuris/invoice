6328367c2a4c591a58ca3ffe6188a1aaa4ca1cbe
Khoa Lam <lakhoa1989@gmail.com>
2021-01-04 07:48:19 +0700

    add NghiepThienSinh

---
+++ src/pages/nghiepthiensinh/copy.bat
@@ -0,0 +1,10 @@
+set /p rootPublish=<..\..\..\copy.cfg
+md "%rootPublish%\210104_NghiepThienSinh_V5_AMLieu"
+copy "..\..\..\dist\nghiepthiensinh\nghiepthiensinh.xml" "%rootPublish%\210104_NghiepThienSinh_V5_AMLieu"
+copy "..\..\..\dist\nghiepthiensinh\nghiepthiensinh.xslt" "%rootPublish%\210104_NghiepThienSinh_V5_AMLieu"
+echo Set oWS = WScript.CreateObject("WScript.Shell")>CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut("210104_NghiepThienSinh_V5_AMLieu.lnk")>>CreateShortcut.vbs
+echo oLink.TargetPath = "%rootPublish%\210104_NghiepThienSinh_V5_AMLieu">>CreateShortcut.vbs
+echo oLink.Save>>CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
