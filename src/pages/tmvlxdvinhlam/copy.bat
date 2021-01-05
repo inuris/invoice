b9bfa2fe941b53bbceb0b798a90e761cedf28c9d
Khoa Lam <lakhoa1989@gmail.com>
2021-01-05 10:33:19 +0700

    add TMVLXDVinhLam

---
+++ src/pages/tmvlxdvinhlam/copy.bat
@@ -0,0 +1,10 @@
+set /p rootPublish=<..\..\..\copy.cfg
+md "%rootPublish%\210106_TMVLXDVinhLam_V5_AMTan"
+copy "..\..\..\dist\tmvlxdvinhlam\tmvlxdvinhlam.xml" "%rootPublish%\210106_TMVLXDVinhLam_V5_AMTan"
+copy "..\..\..\dist\tmvlxdvinhlam\tmvlxdvinhlam.xslt" "%rootPublish%\210106_TMVLXDVinhLam_V5_AMTan"
+echo Set oWS = WScript.CreateObject("WScript.Shell")>CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut("210106_TMVLXDVinhLam_V5_AMTan.lnk")>>CreateShortcut.vbs
+echo oLink.TargetPath = "%rootPublish%\210106_TMVLXDVinhLam_V5_AMTan">>CreateShortcut.vbs
+echo oLink.Save>>CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
