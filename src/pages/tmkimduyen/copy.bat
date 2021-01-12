aa3af69ca8aa980258fc87de80e89d36bee28b68
Khoa Lam <lakhoa1989@gmail.com>
2021-01-12 21:26:35 +0700

    add KimDuyen

---
+++ src/pages/tmkimduyen/copy.bat
@@ -0,0 +1,10 @@
+set /p rootPublish=<..\..\..\copy.cfg
+md "%rootPublish%\210112_TMKimDuyen_V5_AMLieu"
+copy "..\..\..\dist\tmkimduyen\tmkimduyen.xml" "%rootPublish%\210112_TMKimDuyen_V5_AMLieu"
+copy "..\..\..\dist\tmkimduyen\tmkimduyen.xslt" "%rootPublish%\210112_TMKimDuyen_V5_AMLieu"
+echo Set oWS = WScript.CreateObject("WScript.Shell")>CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut("210112_TMKimDuyen_V5_AMLieu.lnk")>>CreateShortcut.vbs
+echo oLink.TargetPath = "%rootPublish%\210112_TMKimDuyen_V5_AMLieu">>CreateShortcut.vbs
+echo oLink.Save>>CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
