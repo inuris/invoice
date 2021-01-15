22ef075a547e9389272ac1f81bdc144c97039665
Khoa Lam <lakhoa1989@gmail.com>
2021-01-15 10:36:24 +0700

    update TrongNghia

---
+++ src/pages/tmdvtrongnghia/copy.bat
@@ -0,0 +1,10 @@
+set /p rootPublish=<..\..\..\copy.cfg
+md "%rootPublish%\201215_TMDVTrongNghia_V5_AMThuy"
+copy "..\..\..\dist\tmdvtrongnghia\tmdvtrongnghia.xml" "%rootPublish%\201215_TMDVTrongNghia_V5_AMThuy"
+copy "..\..\..\dist\tmdvtrongnghia\tmdvtrongnghia.xslt" "%rootPublish%\201215_TMDVTrongNghia_V5_AMThuy"
+echo Set oWS = WScript.CreateObject("WScript.Shell")>CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut("201215_TMDVTrongNghia_V5_AMThuy.lnk")>>CreateShortcut.vbs
+echo oLink.TargetPath = "%rootPublish%\201215_TMDVTrongNghia_V5_AMThuy">>CreateShortcut.vbs
+echo oLink.Save>>CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
