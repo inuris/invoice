794aae3a8793d000b43927d19533ec7095c9cd9b
Khoa Lam <lakhoa1989@gmail.com>
2021-01-26 09:08:26 +0700

    add PhuongThuan

---
+++ src/pages/phuongthuan/copy.bat
@@ -0,0 +1,10 @@
+set /p rootPublish=<..\..\..\copy.cfg
+md "%rootPublish%\210119_PhuongThuan_V5_AMTan"
+copy "..\..\..\dist\phuongthuan\phuongthuan.xml" "%rootPublish%\210119_PhuongThuan_V5_AMTan"
+copy "..\..\..\dist\phuongthuan\phuongthuan.xslt" "%rootPublish%\210119_PhuongThuan_V5_AMTan"
+echo Set oWS = WScript.CreateObject("WScript.Shell")>CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut("210119_PhuongThuan_V5_AMTan.lnk")>>CreateShortcut.vbs
+echo oLink.TargetPath = "%rootPublish%\210119_PhuongThuan_V5_AMTan">>CreateShortcut.vbs
+echo oLink.Save>>CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
