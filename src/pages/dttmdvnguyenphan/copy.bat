4024ef5e7033059f3f24ea020010deb786351a56
Khoa Lam <lakhoa1989@gmail.com>
2021-01-29 08:39:40 +0700

    add NguyenPhan

---
+++ src/pages/dttmdvnguyenphan/copy.bat
@@ -0,0 +1,10 @@
+set /p rootPublish=<..\..\..\copy.cfg
+md "%rootPublish%\210129_DTTMDVNguyenPhan_V5_AMTuanAnh"
+copy "..\..\..\dist\dttmdvnguyenphan\dttmdvnguyenphan.xml" "%rootPublish%\210129_DTTMDVNguyenPhan_V5_AMTuanAnh"
+copy "..\..\..\dist\dttmdvnguyenphan\dttmdvnguyenphan.xslt" "%rootPublish%\210129_DTTMDVNguyenPhan_V5_AMTuanAnh"
+echo Set oWS = WScript.CreateObject("WScript.Shell")>CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut("210129_DTTMDVNguyenPhan_V5_AMTuanAnh.lnk")>>CreateShortcut.vbs
+echo oLink.TargetPath = "%rootPublish%\210129_DTTMDVNguyenPhan_V5_AMTuanAnh">>CreateShortcut.vbs
+echo oLink.Save>>CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
