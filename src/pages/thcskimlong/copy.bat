eca47da58cc049b321542f83b3ef3c8159caa86a
Khoa Lam <lakhoa1989@gmail.com>
2021-02-03 11:53:37 +0700

    add KimLong

---
+++ src/pages/thcskimlong/copy.bat
@@ -0,0 +1,10 @@
+set /p rootPublish=<..\..\..\copy.cfg
+md "%rootPublish%\210203_THCSKimLong_V5_AMBinh"
+copy "..\..\..\dist\thcskimlong\thcskimlong.xml" "%rootPublish%\210203_THCSKimLong_V5_AMBinh"
+copy "..\..\..\dist\thcskimlong\thcskimlong.xslt" "%rootPublish%\210203_THCSKimLong_V5_AMBinh"
+echo Set oWS = WScript.CreateObject("WScript.Shell")>CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut("210203_THCSKimLong_V5_AMBinh.lnk")>>CreateShortcut.vbs
+echo oLink.TargetPath = "%rootPublish%\210203_THCSKimLong_V5_AMBinh">>CreateShortcut.vbs
+echo oLink.Save>>CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
