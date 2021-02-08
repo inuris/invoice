12afe63638fd2b137a441fa0e7837b9009f5f8a4
Khoa Lam <lakhoa1989@gmail.com>
2021-02-08 10:29:29 +0700

    add AnhTram

---
+++ src/pages/anhtram/copy.bat
@@ -0,0 +1,10 @@
+set /p rootPublish=<..\..\..\copy.cfg
+md "%rootPublish%\210208_AnhTram_V5_AMTrong"
+copy "..\..\..\dist\anhtram\anhtram.xml" "%rootPublish%\210208_AnhTram_V5_AMTrong"
+copy "..\..\..\dist\anhtram\anhtram.xslt" "%rootPublish%\210208_AnhTram_V5_AMTrong"
+echo Set oWS = WScript.CreateObject("WScript.Shell")>CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut("210208_AnhTram_V5_AMTrong.lnk")>>CreateShortcut.vbs
+echo oLink.TargetPath = "%rootPublish%\210208_AnhTram_V5_AMTrong">>CreateShortcut.vbs
+echo oLink.Save>>CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
