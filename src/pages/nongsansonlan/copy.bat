ac2178caaf13ac49129c5f95b1bd20d39abb3a5b
Khoa Lam <lakhoa1989@gmail.com>
2021-02-21 22:02:53 +0700

    add NongSan, PCCCLongDat, ThaiPhuAn

---
+++ src/pages/nongsansonlan/copy.bat
@@ -0,0 +1,10 @@
+set /p rootPublish=<..\..\..\copy.cfg
+md "%rootPublish%\210222_NongSanSonLan_V5_AMBinh"
+copy "..\..\..\dist\nongsansonlan\nongsansonlan.xml" "%rootPublish%\210222_NongSanSonLan_V5_AMBinh"
+copy "..\..\..\dist\nongsansonlan\nongsansonlan.xslt" "%rootPublish%\210222_NongSanSonLan_V5_AMBinh"
+echo Set oWS = WScript.CreateObject("WScript.Shell")>CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut("210222_NongSanSonLan_V5_AMBinh.lnk")>>CreateShortcut.vbs
+echo oLink.TargetPath = "%rootPublish%\210222_NongSanSonLan_V5_AMBinh">>CreateShortcut.vbs
+echo oLink.Save>>CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
---
+++ src/pages/sxtmxdpccclongdat/copy.bat
@@ -0,0 +1,10 @@
+set /p rootPublish=<..\..\..\copy.cfg
+md "%rootPublish%\210222_SXTMXDPCCCLongDat_V5_AMBinh"
+copy "..\..\..\dist\sxtmxdpccclongdat\sxtmxdpccclongdat.xml" "%rootPublish%\210222_SXTMXDPCCCLongDat_V5_AMBinh"
+copy "..\..\..\dist\sxtmxdpccclongdat\sxtmxdpccclongdat.xslt" "%rootPublish%\210222_SXTMXDPCCCLongDat_V5_AMBinh"
+echo Set oWS = WScript.CreateObject("WScript.Shell")>CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut("210222_SXTMXDPCCCLongDat_V5_AMBinh.lnk")>>CreateShortcut.vbs
+echo oLink.TargetPath = "%rootPublish%\210222_SXTMXDPCCCLongDat_V5_AMBinh">>CreateShortcut.vbs
+echo oLink.Save>>CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
