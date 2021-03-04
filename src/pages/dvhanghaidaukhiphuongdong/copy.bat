54254c530ea865918eedd6e0d3c49c2d2e514fc0
Khoa Lam <lakhoa1989@gmail.com>
2021-03-04 14:32:54 +0700

    add TamDuc, DauKhiPhuongDong

---
+++ src/pages/dvhanghaidaukhiphuongdong/copy.bat
@@ -0,0 +1,10 @@
+set /p rootPublish=<..\..\..\copy.cfg
+md "%rootPublish%\210304_DVHangHaiDauKhiPhuongDong_V5_AMTrong"
+copy "..\..\..\dist\dvhanghaidaukhiphuongdong\dvhanghaidaukhiphuongdong.xml" "%rootPublish%\210304_DVHangHaiDauKhiPhuongDong_V5_AMTrong"
+copy "..\..\..\dist\dvhanghaidaukhiphuongdong\dvhanghaidaukhiphuongdong.xslt" "%rootPublish%\210304_DVHangHaiDauKhiPhuongDong_V5_AMTrong"
+echo Set oWS = WScript.CreateObject("WScript.Shell")>CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut("210304_DVHangHaiDauKhiPhuongDong_V5_AMTrong.lnk")>>CreateShortcut.vbs
+echo oLink.TargetPath = "%rootPublish%\210304_DVHangHaiDauKhiPhuongDong_V5_AMTrong">>CreateShortcut.vbs
+echo oLink.Save>>CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
---
+++ src/pages/tkxdtamduc/copy.bat
@@ -0,0 +1,10 @@
+set /p rootPublish=<..\..\..\copy.cfg
+md "%rootPublish%\210304_TKXDTamDuc_V5_AMLieu"
+copy "..\..\..\dist\tkxdtamduc\tkxdtamduc.xml" "%rootPublish%\210304_TKXDTamDuc_V5_AMLieu"
+copy "..\..\..\dist\tkxdtamduc\tkxdtamduc.xslt" "%rootPublish%\210304_TKXDTamDuc_V5_AMLieu"
+echo Set oWS = WScript.CreateObject("WScript.Shell")>CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut("210304_TKXDTamDuc_V5_AMLieu.lnk")>>CreateShortcut.vbs
+echo oLink.TargetPath = "%rootPublish%\210304_TKXDTamDuc_V5_AMLieu">>CreateShortcut.vbs
+echo oLink.Save>>CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
