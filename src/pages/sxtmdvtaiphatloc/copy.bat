62720da239b84c4f77fd1a60adcaece30ebd8053
Khoa Lam <lakhoa1989@gmail.com>
2021-03-16 16:09:39 +0700

    add TaiPhatLoc

---
+++ src/pages/sxtmdvtaiphatloc/copy.bat
@@ -0,0 +1,10 @@
+set /p rootPublish=<..\..\..\copy.cfg
+md "%rootPublish%\210316_SXTMDVTaiPhatLoc_V5_AMThuy"
+copy "..\..\..\dist\sxtmdvtaiphatloc\sxtmdvtaiphatloc.xml" "%rootPublish%\210316_SXTMDVTaiPhatLoc_V5_AMThuy"
+copy "..\..\..\dist\sxtmdvtaiphatloc\sxtmdvtaiphatloc.xslt" "%rootPublish%\210316_SXTMDVTaiPhatLoc_V5_AMThuy"
+echo Set oWS = WScript.CreateObject("WScript.Shell")>CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut("210316_SXTMDVTaiPhatLoc_V5_AMThuy.lnk")>>CreateShortcut.vbs
+echo oLink.TargetPath = "%rootPublish%\210316_SXTMDVTaiPhatLoc_V5_AMThuy">>CreateShortcut.vbs
+echo oLink.Save>>CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
