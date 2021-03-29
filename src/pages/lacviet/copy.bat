82e5d49c29735cf1558dc657d0df823f99e6e7b2
Khoa Lam <lakhoa1989@gmail.com>
2021-03-29 15:34:59 +0700

    add LacViet

---
+++ src/pages/lacviet/copy.bat
@@ -0,0 +1,10 @@
+set /p rootPublish=<..\..\..\copy.cfg
+md "%rootPublish%\200619_LacViet_HCM_ImperialGroup_Token_V3_AMTrong"
+copy "..\..\..\dist\lacviet\lacviet.xml" "%rootPublish%\200619_LacViet_HCM_ImperialGroup_Token_V3_AMTrong"
+copy "..\..\..\dist\lacviet\lacviet.xslt" "%rootPublish%\200619_LacViet_HCM_ImperialGroup_Token_V3_AMTrong"
+echo Set oWS = WScript.CreateObject("WScript.Shell")>CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut("200619_LacViet_HCM_ImperialGroup_Token_V3_AMTrong.lnk")>>CreateShortcut.vbs
+echo oLink.TargetPath = "%rootPublish%\200619_LacViet_HCM_ImperialGroup_Token_V3_AMTrong">>CreateShortcut.vbs
+echo oLink.Save>>CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
