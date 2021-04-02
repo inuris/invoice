6ed0c305015b934939e0cd6eb940a322a2e0e2e0
Khoa Lam <lakhoa1989@gmail.com>
2021-04-02 11:09:58 +0700

    add TinThanh

---
+++ src/pages/dtsxtmdvtinthanh/copy.bat
@@ -0,0 +1,10 @@
+set /p rootPublish=<..\..\..\copy.cfg
+md "%rootPublish%\210402_DTSXTMDVTinThanh_V5_AMTuanAnh"
+copy "..\..\..\dist\dtsxtmdvtinthanh\dtsxtmdvtinthanh.xml" "%rootPublish%\210402_DTSXTMDVTinThanh_V5_AMTuanAnh"
+copy "..\..\..\dist\dtsxtmdvtinthanh\dtsxtmdvtinthanh.xslt" "%rootPublish%\210402_DTSXTMDVTinThanh_V5_AMTuanAnh"
+echo Set oWS = WScript.CreateObject("WScript.Shell")>CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut("210402_DTSXTMDVTinThanh_V5_AMTuanAnh.lnk")>>CreateShortcut.vbs
+echo oLink.TargetPath = "%rootPublish%\210402_DTSXTMDVTinThanh_V5_AMTuanAnh">>CreateShortcut.vbs
+echo oLink.Save>>CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
