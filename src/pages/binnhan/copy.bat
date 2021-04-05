2c04ce071d9a004e56e96249219e3e42208839e8
Khoa Lam <lakhoa1989@gmail.com>
2021-04-05 11:09:21 +0700

    add BinNhan

---
+++ src/pages/binnhan/copy.bat
@@ -0,0 +1,10 @@
+set /p rootPublish=<..\..\..\copy.cfg
+md "%rootPublish%\210405_BinNhan_V5_AMTuanAnh"
+copy "..\..\..\dist\binnhan\binnhan.xml" "%rootPublish%\210405_BinNhan_V5_AMTuanAnh"
+copy "..\..\..\dist\binnhan\binnhan.xslt" "%rootPublish%\210405_BinNhan_V5_AMTuanAnh"
+echo Set oWS = WScript.CreateObject("WScript.Shell")>CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut("210405_BinNhan_V5_AMTuanAnh.lnk")>>CreateShortcut.vbs
+echo oLink.TargetPath = "%rootPublish%\210405_BinNhan_V5_AMTuanAnh">>CreateShortcut.vbs
+echo oLink.Save>>CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
