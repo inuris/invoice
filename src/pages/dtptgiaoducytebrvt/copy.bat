8a61234bfcbe3d12a365394dc13806743592df50
Khoa Lam <lakhoa1989@gmail.com>
2021-04-06 14:23:11 +0700

    add GDYT BRVT

---
+++ src/pages/dtptgiaoducytebrvt/copy.bat
@@ -0,0 +1,10 @@
+set /p rootPublish=<..\..\..\copy.cfg
+md "%rootPublish%\210406_DTPTGiaoDucYTeBRVT_V5_AMDuc"
+copy "..\..\..\dist\dtptgiaoducytebrvt\dtptgiaoducytebrvt.xml" "%rootPublish%\210406_DTPTGiaoDucYTeBRVT_V5_AMDuc"
+copy "..\..\..\dist\dtptgiaoducytebrvt\dtptgiaoducytebrvt.xslt" "%rootPublish%\210406_DTPTGiaoDucYTeBRVT_V5_AMDuc"
+echo Set oWS = WScript.CreateObject("WScript.Shell")>CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut("210406_DTPTGiaoDucYTeBRVT_V5_AMDuc.lnk")>>CreateShortcut.vbs
+echo oLink.TargetPath = "%rootPublish%\210406_DTPTGiaoDucYTeBRVT_V5_AMDuc">>CreateShortcut.vbs
+echo oLink.Save>>CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
