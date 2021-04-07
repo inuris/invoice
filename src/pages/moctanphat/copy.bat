821cca744a74d3ef3e3799237236ef445d9b03d9
Khoa Lam <lakhoa1989@gmail.com>
2021-04-07 16:16:48 +0700

    add moctanphat

---
+++ src/pages/moctanphat/copy.bat
@@ -0,0 +1,10 @@
+set /p rootPublish=<..\..\..\copy.cfg
+md "%rootPublish%\210407_MocTanPhat_V5_AMTrong"
+copy "..\..\..\dist\moctanphat\moctanphat.xml" "%rootPublish%\210407_MocTanPhat_V5_AMTrong"
+copy "..\..\..\dist\moctanphat\moctanphat.xslt" "%rootPublish%\210407_MocTanPhat_V5_AMTrong"
+echo Set oWS = WScript.CreateObject("WScript.Shell")>CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut("210407_MocTanPhat_V5_AMTrong.lnk")>>CreateShortcut.vbs
+echo oLink.TargetPath = "%rootPublish%\210407_MocTanPhat_V5_AMTrong">>CreateShortcut.vbs
+echo oLink.Save>>CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
