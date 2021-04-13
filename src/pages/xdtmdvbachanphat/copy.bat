b44c777b0a7d3192ac3aee489c9c7f8ae357cf73
Khoa Lam <lakhoa1989@gmail.com>
2021-04-13 14:46:28 +0700

    add BachAnPhat

---
+++ src/pages/xdtmdvbachanphat/copy.bat
@@ -0,0 +1,10 @@
+set /p rootPublish=<..\..\..\copy.cfg
+md "%rootPublish%\210413_XDTMDVBachAnPhat_V5_AMTuanAnh"
+copy "..\..\..\dist\xdtmdvbachanphat\xdtmdvbachanphat.xml" "%rootPublish%\210413_XDTMDVBachAnPhat_V5_AMTuanAnh"
+copy "..\..\..\dist\xdtmdvbachanphat\xdtmdvbachanphat.xslt" "%rootPublish%\210413_XDTMDVBachAnPhat_V5_AMTuanAnh"
+echo Set oWS = WScript.CreateObject("WScript.Shell")>CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut("210413_XDTMDVBachAnPhat_V5_AMTuanAnh.lnk")>>CreateShortcut.vbs
+echo oLink.TargetPath = "%rootPublish%\210413_XDTMDVBachAnPhat_V5_AMTuanAnh">>CreateShortcut.vbs
+echo oLink.Save>>CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
