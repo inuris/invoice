745a12b7de63b2c12d97b10b75de4b39b7284a6b
Khoa Lam <lakhoa1989@gmail.com>
2021-04-27 09:05:50 +0700

    add DuLichKieuAnh

---
+++ src/pages/dulichkieuanh/copy.bat
@@ -0,0 +1,10 @@
+set /p rootPublish=<..\..\..\copy.cfg
+md "%rootPublish%\210427_DuLichKieuAnh_V5_AMTrong"
+copy "..\..\..\dist\dulichkieuanh\dulichkieuanh.xml" "%rootPublish%\210427_DuLichKieuAnh_V5_AMTrong"
+copy "..\..\..\dist\dulichkieuanh\dulichkieuanh.xslt" "%rootPublish%\210427_DuLichKieuAnh_V5_AMTrong"
+echo Set oWS = WScript.CreateObject("WScript.Shell")>CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut("210427_DuLichKieuAnh_V5_AMTrong.lnk")>>CreateShortcut.vbs
+echo oLink.TargetPath = "%rootPublish%\210427_DuLichKieuAnh_V5_AMTrong">>CreateShortcut.vbs
+echo oLink.Save>>CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
