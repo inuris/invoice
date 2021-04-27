67385f64265791e38cd5fef40a46b2c1eabc2afe
Khoa Lam <lakhoa1989@gmail.com>
2021-04-27 07:55:51 +0700

    add Keri

---
+++ src/pages/tmdvkeri/copy.bat
@@ -0,0 +1,10 @@
+set /p rootPublish=<..\..\..\copy.cfg
+md "%rootPublish%\210426_TMDVKeri_V5_AMOanh"
+copy "..\..\..\dist\tmdvkeri\tmdvkeri.xml" "%rootPublish%\210426_TMDVKeri_V5_AMOanh"
+copy "..\..\..\dist\tmdvkeri\tmdvkeri.xslt" "%rootPublish%\210426_TMDVKeri_V5_AMOanh"
+echo Set oWS = WScript.CreateObject("WScript.Shell")>CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut("210426_TMDVKeri_V5_AMOanh.lnk")>>CreateShortcut.vbs
+echo oLink.TargetPath = "%rootPublish%\210426_TMDVKeri_V5_AMOanh">>CreateShortcut.vbs
+echo oLink.Save>>CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
