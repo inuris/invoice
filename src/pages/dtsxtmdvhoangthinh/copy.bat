4db1b1bea225b0c4022a95ab6294d96004a75878
Khoa Lam <lakhoa1989@gmail.com>
2021-05-20 08:16:18 +0700

    add HoangThinh

---
+++ src/pages/dtsxtmdvhoangthinh/copy.bat
@@ -0,0 +1,10 @@
+set /p rootPublish=<..\..\..\copy.cfg
+md "%rootPublish%\210520_DTSXTMDVHoangThinh_V5_AMDuc"
+copy "..\..\..\dist\dtsxtmdvhoangthinh\dtsxtmdvhoangthinh.xml" "%rootPublish%\210520_DTSXTMDVHoangThinh_V5_AMDuc"
+copy "..\..\..\dist\dtsxtmdvhoangthinh\dtsxtmdvhoangthinh.xslt" "%rootPublish%\210520_DTSXTMDVHoangThinh_V5_AMDuc"
+echo Set oWS = WScript.CreateObject("WScript.Shell")>CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut("210520_DTSXTMDVHoangThinh_V5_AMDuc.lnk")>>CreateShortcut.vbs
+echo oLink.TargetPath = "%rootPublish%\210520_DTSXTMDVHoangThinh_V5_AMDuc">>CreateShortcut.vbs
+echo oLink.Save>>CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
