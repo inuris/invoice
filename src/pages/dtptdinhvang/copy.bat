7206ae4a0f8ef166bd59e745396897afffa9da0b
Khoa Lam <lakhoa1989@gmail.com>
2021-05-28 04:51:55 +0700

    add DinhVang

---
+++ src/pages/dtptdinhvang/copy.bat
@@ -0,0 +1,10 @@
+set /p rootPublish=<..\..\..\copy.cfg
+md "%rootPublish%\210527_DTPTDinhVang_V5_AMTan"
+copy "..\..\..\dist\dtptdinhvang\dtptdinhvang.xml" "%rootPublish%\210527_DTPTDinhVang_V5_AMTan"
+copy "..\..\..\dist\dtptdinhvang\dtptdinhvang.xslt" "%rootPublish%\210527_DTPTDinhVang_V5_AMTan"
+echo Set oWS = WScript.CreateObject("WScript.Shell")>CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut("210527_DTPTDinhVang_V5_AMTan.lnk")>>CreateShortcut.vbs
+echo oLink.TargetPath = "%rootPublish%\210527_DTPTDinhVang_V5_AMTan">>CreateShortcut.vbs
+echo oLink.Save>>CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
