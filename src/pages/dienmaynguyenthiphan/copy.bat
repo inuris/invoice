545d66fa064edfdcd11fb8f23598fe614e06a243
Khoa Lam <lakhoa1989@gmail.com>
2021-06-02 08:46:42 +0700

    add DienmayNguyenThiPhan

---
+++ src/pages/dienmaynguyenthiphan/copy.bat
@@ -0,0 +1,10 @@
+set /p rootPublish=<..\..\..\copy.cfg
+md "%rootPublish%\210602_DienMayNguyenThiPhan_V5_AMTan"
+copy "..\..\..\dist\dienmaynguyenthiphan\dienmaynguyenthiphan.xml" "%rootPublish%\210602_DienMayNguyenThiPhan_V5_AMTan"
+copy "..\..\..\dist\dienmaynguyenthiphan\dienmaynguyenthiphan.xslt" "%rootPublish%\210602_DienMayNguyenThiPhan_V5_AMTan"
+echo Set oWS = WScript.CreateObject("WScript.Shell")>CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut("210602_DienMayNguyenThiPhan_V5_AMTan.lnk")>>CreateShortcut.vbs
+echo oLink.TargetPath = "%rootPublish%\210602_DienMayNguyenThiPhan_V5_AMTan">>CreateShortcut.vbs
+echo oLink.Save>>CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
