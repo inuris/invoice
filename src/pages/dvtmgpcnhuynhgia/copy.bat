febed02697a5a73400f82d523a99ffdf9368ae23
Khoa Lam <lakhoa1989@gmail.com>
2021-06-02 10:36:53 +0700

    add HuynhGia

---
+++ src/pages/dvtmgpcnhuynhgia/copy.bat
@@ -0,0 +1,10 @@
+set /p rootPublish=<..\..\..\copy.cfg
+md "%rootPublish%\210603_DVTMGPCNHuynhGia_V5_AMHuong"
+copy "..\..\..\dist\dvtmgpcnhuynhgia\dvtmgpcnhuynhgia.xml" "%rootPublish%\210603_DVTMGPCNHuynhGia_V5_AMHuong"
+copy "..\..\..\dist\dvtmgpcnhuynhgia\dvtmgpcnhuynhgia.xslt" "%rootPublish%\210603_DVTMGPCNHuynhGia_V5_AMHuong"
+echo Set oWS = WScript.CreateObject("WScript.Shell")>CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut("210603_DVTMGPCNHuynhGia_V5_AMHuong.lnk")>>CreateShortcut.vbs
+echo oLink.TargetPath = "%rootPublish%\210603_DVTMGPCNHuynhGia_V5_AMHuong">>CreateShortcut.vbs
+echo oLink.Save>>CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
