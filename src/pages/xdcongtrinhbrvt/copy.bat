6261ac2fd3306fb8ff23edb5ba115b0b31a201e5
Khoa Lam <lakhoa1989@gmail.com>
2021-06-02 09:42:43 +0700

    add XDCongTrinhBRVT

---
+++ src/pages/xdcongtrinhbrvt/copy.bat
@@ -0,0 +1,10 @@
+set /p rootPublish=<..\..\..\copy.cfg
+md "%rootPublish%\210526_XDCongTrinhBRVT_V5_AMChi"
+copy "..\..\..\dist\xdcongtrinhbrvt\xdcongtrinhbrvt.xml" "%rootPublish%\210526_XDCongTrinhBRVT_V5_AMChi"
+copy "..\..\..\dist\xdcongtrinhbrvt\xdcongtrinhbrvt.xslt" "%rootPublish%\210526_XDCongTrinhBRVT_V5_AMChi"
+echo Set oWS = WScript.CreateObject("WScript.Shell")>CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut("210526_XDCongTrinhBRVT_V5_AMChi.lnk")>>CreateShortcut.vbs
+echo oLink.TargetPath = "%rootPublish%\210526_XDCongTrinhBRVT_V5_AMChi">>CreateShortcut.vbs
+echo oLink.Save>>CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
