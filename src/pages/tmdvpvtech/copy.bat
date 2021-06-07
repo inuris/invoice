66e3ef2efd940b6e18fc488416ca5484293e254d
Khoa Lam <lakhoa1989@gmail.com>
2021-06-08 06:50:46 +0700

    add PVTech

---
+++ src/pages/tmdvpvtech/copy.bat
@@ -0,0 +1,10 @@
+set /p rootPublish=<..\..\..\copy.cfg
+md "%rootPublish%\210610_TMDVPVTech_V5_AMMinh"
+copy "..\..\..\dist\tmdvpvtech\tmdvpvtech.xml" "%rootPublish%\210610_TMDVPVTech_V5_AMMinh"
+copy "..\..\..\dist\tmdvpvtech\tmdvpvtech.xslt" "%rootPublish%\210610_TMDVPVTech_V5_AMMinh"
+echo Set oWS = WScript.CreateObject("WScript.Shell")>CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut("210610_TMDVPVTech_V5_AMMinh.lnk")>>CreateShortcut.vbs
+echo oLink.TargetPath = "%rootPublish%\210610_TMDVPVTech_V5_AMMinh">>CreateShortcut.vbs
+echo oLink.Save>>CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
