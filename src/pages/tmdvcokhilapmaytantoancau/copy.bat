7ffd37daed573fb1e04ae565ee1b27617dbde6fd
Khoa Lam <lakhoa1989@gmail.com>
2021-06-08 06:20:26 +0700

    add TanToanCau

---
+++ src/pages/tmdvcokhilapmaytantoancau/copy.bat
@@ -0,0 +1,10 @@
+set /p rootPublish=<..\..\..\copy.cfg
+md "%rootPublish%\210608_TMDVCoKhiLapMayTanToanCau_V5_AMQHuong"
+copy "..\..\..\dist\tmdvcokhilapmaytantoancau\tmdvcokhilapmaytantoancau.xml" "%rootPublish%\210608_TMDVCoKhiLapMayTanToanCau_V5_AMQHuong"
+copy "..\..\..\dist\tmdvcokhilapmaytantoancau\tmdvcokhilapmaytantoancau.xslt" "%rootPublish%\210608_TMDVCoKhiLapMayTanToanCau_V5_AMQHuong"
+echo Set oWS = WScript.CreateObject("WScript.Shell")>CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut("210608_TMDVCoKhiLapMayTanToanCau_V5_AMQHuong.lnk")>>CreateShortcut.vbs
+echo oLink.TargetPath = "%rootPublish%\210608_TMDVCoKhiLapMayTanToanCau_V5_AMQHuong">>CreateShortcut.vbs
+echo oLink.Save>>CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
