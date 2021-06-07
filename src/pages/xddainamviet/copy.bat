3dfe12f5000fed8423516a6abae420005aa5c0c4
Khoa Lam <lakhoa1989@gmail.com>
2021-06-08 06:10:44 +0700

    add DaiNamViet

---
+++ src/pages/xddainamviet/copy.bat
@@ -0,0 +1,10 @@
+set /p rootPublish=<..\..\..\copy.cfg
+md "%rootPublish%\210608_XDDaiNamViet_V5_AMTuanAnh"
+copy "..\..\..\dist\xddainamviet\xddainamviet.xml" "%rootPublish%\210608_XDDaiNamViet_V5_AMTuanAnh"
+copy "..\..\..\dist\xddainamviet\xddainamviet.xslt" "%rootPublish%\210608_XDDaiNamViet_V5_AMTuanAnh"
+echo Set oWS = WScript.CreateObject("WScript.Shell")>CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut("210608_XDDaiNamViet_V5_AMTuanAnh.lnk")>>CreateShortcut.vbs
+echo oLink.TargetPath = "%rootPublish%\210608_XDDaiNamViet_V5_AMTuanAnh">>CreateShortcut.vbs
+echo oLink.Save>>CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
