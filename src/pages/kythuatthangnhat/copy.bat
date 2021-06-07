7b79b738e761ffab7d4956b57a85c1df14f7eb8c
Khoa Lam <lakhoa1989@gmail.com>
2021-06-08 06:33:35 +0700

    add KTThangNhat

---
+++ src/pages/kythuatthangnhat/copy.bat
@@ -0,0 +1,10 @@
+set /p rootPublish=<..\..\..\copy.cfg
+md "%rootPublish%\210609_KyThuatThangNhat_V5_AMQHuong"
+copy "..\..\..\dist\kythuatthangnhat\kythuatthangnhat.xml" "%rootPublish%\210609_KyThuatThangNhat_V5_AMQHuong"
+copy "..\..\..\dist\kythuatthangnhat\kythuatthangnhat.xslt" "%rootPublish%\210609_KyThuatThangNhat_V5_AMQHuong"
+echo Set oWS = WScript.CreateObject("WScript.Shell")>CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut("210609_KyThuatThangNhat_V5_AMQHuong.lnk")>>CreateShortcut.vbs
+echo oLink.TargetPath = "%rootPublish%\210609_KyThuatThangNhat_V5_AMQHuong">>CreateShortcut.vbs
+echo oLink.Save>>CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
