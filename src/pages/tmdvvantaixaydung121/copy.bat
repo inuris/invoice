b897c0e2e23edc662764c5de641b948b3fd6e141
Khoa Lam <lakhoa1989@gmail.com>
2021-06-08 06:43:38 +0700

    add 121

---
+++ src/pages/tmdvvantaixaydung121/copy.bat
@@ -0,0 +1,10 @@
+set /p rootPublish=<..\..\..\copy.cfg
+md "%rootPublish%\210609_TMDVVanTaiXayDung121_V5_AMOanh"
+copy "..\..\..\dist\tmdvvantaixaydung121\tmdvvantaixaydung121.xml" "%rootPublish%\210609_TMDVVanTaiXayDung121_V5_AMOanh"
+copy "..\..\..\dist\tmdvvantaixaydung121\tmdvvantaixaydung121.xslt" "%rootPublish%\210609_TMDVVanTaiXayDung121_V5_AMOanh"
+echo Set oWS = WScript.CreateObject("WScript.Shell")>CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut("210609_TMDVVanTaiXayDung121_V5_AMOanh.lnk")>>CreateShortcut.vbs
+echo oLink.TargetPath = "%rootPublish%\210609_TMDVVanTaiXayDung121_V5_AMOanh">>CreateShortcut.vbs
+echo oLink.Save>>CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
