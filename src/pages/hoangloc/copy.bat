98451a43850421900433eaad7209d80304726c3f
Khoa Lam <lakhoa1989@gmail.com>
2021-06-08 08:42:09 +0700

    add HoangLoc

---
+++ src/pages/hoangloc/copy.bat
@@ -0,0 +1,10 @@
+set /p rootPublish=<..\..\..\copy.cfg
+md "%rootPublish%\210603_HoangLoc_V5_AMChi"
+copy "..\..\..\dist\hoangloc\hoangloc.xml" "%rootPublish%\210603_HoangLoc_V5_AMChi"
+copy "..\..\..\dist\hoangloc\hoangloc.xslt" "%rootPublish%\210603_HoangLoc_V5_AMChi"
+echo Set oWS = WScript.CreateObject("WScript.Shell")>CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut("210603_HoangLoc_V5_AMChi.lnk")>>CreateShortcut.vbs
+echo oLink.TargetPath = "%rootPublish%\210603_HoangLoc_V5_AMChi">>CreateShortcut.vbs
+echo oLink.Save>>CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
