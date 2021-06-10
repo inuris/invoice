bf63f2ed196261b234e462fd5553b6bb6b6e3381
Khoa Lam <lakhoa1989@gmail.com>
2021-06-11 06:22:14 +0700

    add DK

---
+++ src/pages/tvtkxddangkhoa/copy.bat
@@ -0,0 +1,10 @@
+set /p rootPublish=<..\..\..\copy.cfg
+md "%rootPublish%\210614_TVTKXDDangKhoa_V5_AMTuanAnh"
+copy "..\..\..\dist\tvtkxddangkhoa\tvtkxddangkhoa.xml" "%rootPublish%\210614_TVTKXDDangKhoa_V5_AMTuanAnh"
+copy "..\..\..\dist\tvtkxddangkhoa\tvtkxddangkhoa.xslt" "%rootPublish%\210614_TVTKXDDangKhoa_V5_AMTuanAnh"
+echo Set oWS = WScript.CreateObject("WScript.Shell")>CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut("210614_TVTKXDDangKhoa_V5_AMTuanAnh.lnk")>>CreateShortcut.vbs
+echo oLink.TargetPath = "%rootPublish%\210614_TVTKXDDangKhoa_V5_AMTuanAnh">>CreateShortcut.vbs
+echo oLink.Save>>CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
