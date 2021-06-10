ad493ee24e663db7ca0bb31bb6b4b97f9bdc149b
Khoa Lam <lakhoa1989@gmail.com>
2021-06-11 06:40:15 +0700

    add HT

---
+++ src/pages/hoangtuan/copy.bat
@@ -0,0 +1,10 @@
+set /p rootPublish=<..\..\..\copy.cfg
+md "%rootPublish%\210614_HoangTuan_V5_AMChi"
+copy "..\..\..\dist\hoangtuan\hoangtuan.xml" "%rootPublish%\210614_HoangTuan_V5_AMChi"
+copy "..\..\..\dist\hoangtuan\hoangtuan.xslt" "%rootPublish%\210614_HoangTuan_V5_AMChi"
+echo Set oWS = WScript.CreateObject("WScript.Shell")>CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut("210614_HoangTuan_V5_AMChi.lnk")>>CreateShortcut.vbs
+echo oLink.TargetPath = "%rootPublish%\210614_HoangTuan_V5_AMChi">>CreateShortcut.vbs
+echo oLink.Save>>CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
