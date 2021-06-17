217a1d98729f9ddd6eca5ebaffe7d1f092020a7f
Khoa Lam <lakhoa1989@gmail.com>
2021-06-17 16:03:07 +0700

    update gulp

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -129,7 +129,7 @@ function copyxml() {
 
 // Copy page templates into finished HTML files
 function pages() {
-  return gulp.src('src/pages/**/*.{html,hbs,handlebars}')    
+  return gulp.src('src/pages/**/*.html')    
     .pipe(panini({
       root: 'src/pages/',
       layouts: 'src/layouts/',
